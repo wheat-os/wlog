19424aef47c9e8d3a61add0f0e2712c82e119c20
13886334215 <1762386214@qq.com>
2022-04-04 18:47:34 +0800

    <test>[example]:<create example test>

---
+++ example/logger_example_test.go
@@ -0,0 +1,22 @@
+package example
+
+import (
+	"gitee.com/wheat-os/wlog"
+	"os"
+	"testing"
+)
+
+func TestExample(t *testing.T) {
+	log := wlog.New()
+
+	file, err := os.OpenFile("log.text", os.O_WRONLY|os.O_CREATE, 0775)
+	if err != nil {
+		return
+	}
+
+	log.SetOptions(wlog.WithOutput(file))
+
+	log.Info("123", 123, map[string]int{"qqq": 111})
+	log.Debug("123", 123, map[string]int{"qqq": 111})
+
+}
