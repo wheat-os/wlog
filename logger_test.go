833117e1cf278a93e0f38e43a8aa03b437d107df
bandl <1658002533@qq.com>
2022-04-05 16:43:43 +0800

    test: add logger test

--- logger_test.go
+++ logger_test.go
@@ -1,6 +1,7 @@
 package wlog
 
 import (
+	"errors"
 	"testing"
 )
 
@@ -10,4 +11,15 @@ func Test_logger_Debug(t *testing.T) {
 
 	log := New()
 	log.Info(1111)
+}
+
+func Test_logger(t *testing.T) {
+	logger := New(WithFormatter(JsonFormatter(false)))
+	logger.Debug("111", "222", errors.New("err is open"))
+	logger.SetOptions(WithFormatter(JsonFormatter(true)))
+	logger.SetOptions(WithDisPlayLevel(ErrorLevel))
+
+	logger.Info("错误")
+	logger.Error("发生错误")
+	logger.Error("发生错误2")
 }
