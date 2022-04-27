65c991b8953d2364e2741aac42cd7c459c5bafd4
13886334215 <1762386214@qq.com>
2022-04-27 11:37:39 +0800

    fix:fix color param,add set font color func

--- color_test.go
+++ color_test.go
@@ -1,31 +1,19 @@
 package wlog
 
 import (
-	"fmt"
-	"github.com/fatih/color"
 	"testing"
 )
 
+// 普通输出和日志输出
 func TestColor(t *testing.T) {
-	DebugColor.Println("1111")
-	InfoColor.Println("111")
-	WarnColor.Println("111")
-	ErrorColor.Println("111")
-	PanicColor.Println("111")
-	FatalColor.Println("111")
+	debugColor.Println("1111")
+	infoColor.Println("111")
+	Info("dwdsadw")
 }
 
-func TestColorWrite(t *testing.T) {
-	sprintf := DebugColor.Color.Sprintf("%s\n", "xxx")
-	fmt.Println(sprintf)
-	sprintln := DebugColor.Color.Sprintln("111")
-	fmt.Println(sprintln)
-	sprint := InfoColor.Color.Sprint("111\n")
-	fmt.Println(sprint)
+func TestChangeLogColor(t *testing.T) {
+	Info("11111")
+	SetStdOptions(SetLogLevelColor(InfoLevel, FgHiYellow))
 
-	colorer := logColors[InfoLevel]
-	colorer.Println(111)
-	InfoColor.SetFontColor(color.FgRed)
-	//colorer.SetFontColor(color.FgRed)
-	colorer.Println(111)
+	Info("1111")
 }
